706e3dbff98a393af953135c2279292ce0da15e7
Starship100 <jimmy.widlund@outlook.com>
2026-03-07 14:12:31 +0100

    OOP

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
-luhfuf
+""""
+class SafeStorage:
+    __data = None
+    def get(self):
+        return self.__data
+    def put(self, data):
+        self.__data = data
+
+safe = SafeStorage()
+safe.put("Anakonda")
+x = safe.get()
+safe.put("Boaorm")
+y = safe.get()
+print(x, y)"""
+
+class Animal:
+    def make_noise(self):
+        print("Detta djur har vi inget ljud för.")
+
+class Rooster(Animal):
+    def make_noise(self):
+        print("UhhhhhUhhUhhUuuuuuuuuhh!")
+
+class Dog(Animal):
+    def make_noise(self):
+        print("Voff!")
+
+class Cat(Animal):
+    def make_noise(shelf):
+        super().make_noise()
+        print("Mjau!")
+
+def sound_off(animal):
+    animal.make_noise()
+
+c = Cat()
+d = Dog()
+h = Rooster()
+Animal.make_noise([c, d, h])
